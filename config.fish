12badc97d487df5ecce2e50a2b86cbbac7526686
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-06-14 10:43:42 +1000

    use vim as the man pager

--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
+# vim: nospell
+
 set -x LESS_TERMCAP_mb (printf "\033[01;31m")
 set -x LESS_TERMCAP_md (printf "\033[01;31m")
 set -x LESS_TERMCAP_me (printf "\033[0m")
@@ -5,5 +7,7 @@ set -x LESS_TERMCAP_se (printf "\033[0m")
 set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\033[0m")
 set -x LESS_TERMCAP_us (printf "\033[01;32m")
+
+set -x MANPAGER "nvim -c 'set filetype=man nospell' -"
 
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
