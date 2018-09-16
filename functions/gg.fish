2ac5f7d1a8bad49d18ce4b4e78fc7503fb7e629c
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-09-16 11:15:05 +0200

    update gg.fish

--- functions/gg.fish
+++ functions/gg.fish
@@ -1,8 +1,8 @@
 function gg --description "ghcid --command='stack ghci' [module]"
-  if test (count  $argv) -gt 0
+  if test (count $argv) -eq 1; and test -f "$argv[1]"
     ghcid --command="stack ghci --no-load --no-build --ghci-options $argv"
   else
-    ghcid --command="stack ghci"
+    ghcid --command="stack ghci $argv"
   end
 end
 
